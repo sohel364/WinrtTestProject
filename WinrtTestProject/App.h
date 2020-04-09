ee3cbfd711fcf3db20881608bba46ce5b6a4c9e7
sohel364 <sohel.official@gmail.com>
2020-04-10 02:05:26 +0600

    Checkin for suggestion with marshalling error which happens on c++/winrt but not on c++/cx

---
+++ WinrtTestProject/App.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::WinrtTestProject::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+
+        void LoadData() {};
+        void InitAsync();
+        Windows::UI::Xaml::DispatcherTimer CreateAndStartDispatcherTimer(std::chrono::milliseconds interval, void(App::* onTick)(IInspectable sender, IInspectable e));
+
+    private:
+        void OnTimerTick(IInspectable sensder, IInspectable e) {};
+
+    private:
+        Windows::UI::Xaml::DispatcherTimer m_timer;
+    };
+}
